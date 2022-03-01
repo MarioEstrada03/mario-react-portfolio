fa59d72f851d81b4561be036130f74a610d7ac21
MarioEstrada03 <98118582+MarioEstrada03@users.noreply.github.com>
2022-03-01 13:28:09 -0800

    added navigaion pages

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Mario Estrada Portfolio</h1>
-        <div><h2>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h2></div>
+       
+
+        <Router>
+        <div>
+        <h1>Mario Estrada -  Portfolio</h1>
+        <div><h3>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h3></div>
+        <NavigationContainer />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about-me" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+        </div>
+        </Router>
+
       </div>
     );
   }
